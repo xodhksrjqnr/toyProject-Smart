6b479ec633ed6cc4c5e955490abb622b4dad1358
xodhksrjqnr <xodhksrjqnr@naver.com>
2023-01-03 21:49:51 +0900

    #2 memberService 인터페이스 & memberServiceImpl 구현

--- src/main/java/taewan/Smart/member/service/MemberServiceImpl.java
+++ src/main/java/taewan/Smart/member/service/MemberServiceImpl.java
@@ -1,4 +1,53 @@
 package taewan.Smart.member.service;
 
-public class MemberService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import taewan.Smart.member.dto.MemberInfoDto;
+import taewan.Smart.member.dto.MemberSaveDto;
+import taewan.Smart.member.dto.MemberUpdateDto;
+import taewan.Smart.member.entity.Member;
+import taewan.Smart.member.repository.MemberRepository;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class MemberServiceImpl implements MemberService {
+
+    private MemberRepository memberRepository;
+
+    @Autowired
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Override
+    public MemberInfoDto findOne(Long memberId) {
+        return new MemberInfoDto(memberRepository.findById(memberId).orElseThrow());
+    }
+
+    @Override
+    public List<MemberInfoDto> findAll() {
+        return Arrays.asList(memberRepository.findAll()
+                .stream().map(MemberInfoDto::new)
+                .toArray(MemberInfoDto[]::new));
+    }
+
+    @Override
+    public Long save(MemberSaveDto memberSaveDto) {
+        return memberRepository.save(new Member(memberSaveDto)).getMemberId();
+    }
+
+    @Override
+    public Long modify(Long memberId, MemberUpdateDto memberUpdateDto) {
+        Member found = memberRepository.findById(memberId).orElseThrow();
+
+        found.updateMember(memberUpdateDto);
+        return memberId;
+    }
+
+    @Override
+    public void delete(Long memberId) {
+        memberRepository.deleteById(memberId);
+    }
 }
