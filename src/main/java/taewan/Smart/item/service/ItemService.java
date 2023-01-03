d3267ee5d3504b21157800ec29bf09659136ed4f
xodhksrjqnr <xodhksrjqnr@naver.com>
2023-01-03 20:33:18 +0900

    itemService 인터페이스 & itemServiceImpl 구현

--- src/main/java/taewan/Smart/item/service/ItemService.java
+++ src/main/java/taewan/Smart/item/service/ItemService.java
@@ -1,4 +1,16 @@
 package taewan.Smart.item.service;
 
-public class ItemService {
+import taewan.Smart.item.entity.ItemInfoDto;
+import taewan.Smart.item.entity.ItemSaveDto;
+import taewan.Smart.item.entity.ItemUpdateDto;
+
+import java.util.List;
+
+public interface ItemService {
+
+    ItemInfoDto findOne(Long itemId);
+    List<ItemInfoDto> findAll();
+    Long save(ItemSaveDto itemSaveDto);
+    Long modify(Long itemId, ItemUpdateDto itemUpdateDto);
+    void delete(Long itemId);
 }
---
+++ src/main/java/taewan/Smart/item/service/ItemServiceImpl.java
@@ -0,0 +1,52 @@
+package taewan.Smart.item.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import taewan.Smart.item.entity.Item;
+import taewan.Smart.item.entity.ItemInfoDto;
+import taewan.Smart.item.entity.ItemSaveDto;
+import taewan.Smart.item.entity.ItemUpdateDto;
+import taewan.Smart.item.repository.ItemRepository;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class ItemServiceImpl implements ItemService {
+    private ItemRepository itemRepository;
+
+    @Autowired
+    public ItemServiceImpl(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+
+    @Override
+    public ItemInfoDto findOne(Long itemId) {
+        return new ItemInfoDto(itemRepository.findById(itemId).orElseThrow());
+    }
+
+    @Override
+    public List<ItemInfoDto> findAll() {
+        return Arrays.asList(itemRepository.findAll()
+                .stream().map(ItemInfoDto::new)
+                .toArray(ItemInfoDto[]::new));
+    }
+
+    @Override
+    public Long save(ItemSaveDto itemSaveDto) {
+        return itemRepository.save(new Item(itemSaveDto)).getItemId();
+    }
+
+    @Override
+    public Long modify(Long itemId, ItemUpdateDto itemUpdateDto) {
+        Item found = itemRepository.findById(itemId).orElseThrow();
+
+        found.updateItem(itemUpdateDto);
+        return itemId;
+    }
+
+    @Override
+    public void delete(Long itemId) {
+        itemRepository.deleteById(itemId);
+    }
+}
