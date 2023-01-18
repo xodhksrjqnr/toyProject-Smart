9a70dc88f0353f5c51e8a8cb3045940d66816ba5
presentKey <kim.hyeonjae4@gmail.com>
2023-01-19 02:00:57 +0900

    refactor: product api 호출 개선

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import SideNav from './components/SideNav';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ProductApiProvider } from './context/ProductApiContext';
 
 const queryClient = new QueryClient();
 
@@ -11,9 +12,11 @@ function App() {
       <Header />
       <main className="flex justify-center max-w-screen-2xl w-full">
         <SideNav />
-        <QueryClientProvider client={queryClient}>
-          <Outlet />
-        </QueryClientProvider>
+        <ProductApiProvider>
+          <QueryClientProvider client={queryClient}>
+            <Outlet />
+          </QueryClientProvider>
+        </ProductApiProvider>
       </main>
     </>
   );
