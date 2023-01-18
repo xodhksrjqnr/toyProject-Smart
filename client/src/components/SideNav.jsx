d4eff466de14336b467104d34dab7bb8d738da1b
presentKey <kim.hyeonjae4@gmail.com>
2023-01-19 02:03:02 +0900

    refactor: categories 변수를 상수 파일로 추출

--- client/src/components/SideNav.jsx
+++ client/src/components/SideNav.jsx
@@ -1,12 +1,7 @@
 import React, { useState } from 'react';
 import { BsCaretDownFill } from 'react-icons/bs';
+import { categories } from '../constants/categories';
 
-const categories = [
-  { main: '상의', subdivision: ['후드 티셔츠', '맨투맨'] },
-  { main: '아우터', subdivision: ['코트', '패딩'] },
-  { main: '바지', subdivision: ['트레이닝 팬츠', '숏팬츠'] },
-  { main: '신발', subdivision: ['구두', '운동화'] },
-];
 export default function SideNav() {
   const handleClick = (e) => {
     e.target.nextSibling.classList.toggle('hidden');
