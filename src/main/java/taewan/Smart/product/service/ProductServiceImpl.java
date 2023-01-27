4982e89a92e1ec91caa40904de781615bfbb1d0b
xodhksrjqnr <xodhksrjqnr@naver.com>
2023-01-27 22:28:15 +0900

    #3 Product 도메인 file 관련 기능 분리 ProductService file 저장 및 조회 관련 기능 제거 FileUtils file 저장 및 조회 관련 기능 구현

--- src/main/java/taewan/Smart/product/service/ProductServiceImpl.java
+++ src/main/java/taewan/Smart/product/service/ProductServiceImpl.java
@@ -6,30 +6,21 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
 import taewan.Smart.product.entity.Product;
 import taewan.Smart.product.dto.ProductInfoDto;
 import taewan.Smart.product.dto.ProductSaveDto;
 import taewan.Smart.product.dto.ProductUpdateDto;
 import taewan.Smart.product.repository.ProductRepository;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+import static taewan.Smart.util.FileUtils.*;
 
 @Service
 public class ProductServiceImpl implements ProductService {
     private ProductRepository productRepository;
-    @Value("${resource.save.path}")
-    private String resourceSavePath;
-    @Value("${server.address}")
-    private String serverAddress;
-    @Value("${server.port}")
-    private String serverPort;
+    @Value("${root.path}")
+    private String root;
+    @Value("${server.address.basic}")
+    private String address;
 
     @Autowired
     public ProductServiceImpl(ProductRepository productRepository) {
@@ -40,29 +31,30 @@ public class ProductServiceImpl implements ProductService {
     public ProductInfoDto findOne(Long productId) {
         Product found = productRepository.findById(productId).orElseThrow();
 
-        return new ProductInfoDto(found, findImgFiles(found.getImgFolderPath()));
+        return new ProductInfoDto(found, findImgFiles(found.getImgFolderPath(), root, address));
     }
 
     @Override
     public Page<ProductInfoDto> findAll(Pageable pageable) {
         return productRepository.findAll(pageable)
-                .map(p -> new ProductInfoDto(p, findImgFiles(p.getImgFolderPath())));
+                .map(p -> new ProductInfoDto(p, findImgFiles(p.getImgFolderPath(), root, address)));
     }
 
     @Override
     public Page<ProductInfoDto> findAllWithFilter(Pageable pageable, String code) {
         return productRepository.findByCodeContaining(pageable, code)
-                .map(p -> new ProductInfoDto(p, findImgFiles(p.getImgFolderPath())));
+                .map(p -> new ProductInfoDto(p, findImgFiles(p.getImgFolderPath(), root, address)));
     }
 
     @Transactional
     @Override
     public Long save(ProductSaveDto productSaveDto) {
         String path = "images/products/" + productSaveDto.getCode() + "/" + productSaveDto.getName();
-        String infoFileName = path + "/" + saveImgFile(productSaveDto.getDetailInfo(), path);
+        String infoFileName = path + "/" + saveImgFile(productSaveDto.getDetailInfo(), root + path);
 
-        saveImgFiles(productSaveDto.getImgFiles(), path + "/view");
-        return productRepository.save(new Product(productSaveDto, path + "/view", infoFileName)).getId();
+        path += "/view";
+        saveImgFiles(productSaveDto.getImgFiles(), root + path);
+        return productRepository.save(new Product(productSaveDto, root + path, infoFileName)).getId();
     }
 
     @Transactional
@@ -80,51 +72,5 @@ public class ProductServiceImpl implements ProductService {
 
         deleteDirectory(directoryPath);
         productRepository.deleteById(productId);
-    }
-
-    private List<String> findImgFiles(String directoryPath) {
-        List<String> found = new ArrayList<>();
-        File dir = new File(resourceSavePath + directoryPath);
-        File[] files = dir.listFiles();
-
-        for (File f : files)
-            if (f.isFile())
-                found.add(serverAddress + ":" + serverPort + "/" + directoryPath + "/" + f.getName());
-        return found;
-    }
-
-    private void saveImgFiles(List<MultipartFile> files, String path) {
-        for (MultipartFile f : files)
-            saveImgFile(f, path);
-    }
-
-    private String saveImgFile(MultipartFile file, String path) {
-        String extension = file.getContentType().replaceFirst(".*/", ".");
-        String uploadName = UUID.randomUUID().toString() + extension;
-
-        try {
-            Files.createDirectories(Paths.get(resourceSavePath + path));
-            file.transferTo(new File(resourceSavePath + path, uploadName));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        return uploadName;
-    }
-
-    private void deleteDirectory(String directoryPath) {
-        File dir = new File(directoryPath);
-        File[] files = dir.listFiles();
-
-        try {
-            for (File f : files) {
-                String target = directoryPath + "/" + f.getName();
-                if (f.isDirectory())
-                    deleteDirectory(target);
-                Files.deleteIfExists(Paths.get(target));
-            }
-            Files.deleteIfExists(Paths.get(directoryPath));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
---
+++ src/main/java/taewan/Smart/util/FileUtils.java
@@ -0,0 +1,60 @@
+package taewan.Smart.util;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class FileUtils {
+
+    public static List<String> findImgFiles(String directoryPath, String root, String address) {
+        List<String> found = new ArrayList<>();
+        File dir = new File(root + directoryPath);
+        File[] files = dir.listFiles();
+
+        for (File f : files)
+            if (f.isFile())
+                found.add(address + directoryPath + "/" + f.getName());
+        return found;
+    }
+
+    public static void saveImgFiles(List<MultipartFile> files, String path) {
+        for (MultipartFile f : files)
+            saveImgFile(f, path);
+    }
+
+    public static String saveImgFile(MultipartFile file, String path) {
+        String extension = file.getContentType().replaceFirst(".*/", ".");
+        String uploadName = UUID.randomUUID().toString() + extension;
+
+        try {
+            Files.createDirectories(Paths.get(path));
+            file.transferTo(new File(path, uploadName));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return uploadName;
+    }
+
+    public static void deleteDirectory(String directoryPath) {
+        File dir = new File(directoryPath);
+        File[] files = dir.listFiles();
+
+        try {
+            for (File f : files) {
+                String target = directoryPath + "/" + f.getName();
+                if (f.isDirectory())
+                    deleteDirectory(target);
+                Files.deleteIfExists(Paths.get(target));
+            }
+            Files.deleteIfExists(Paths.get(directoryPath));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
