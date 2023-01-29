51a98262a250a0704c21539b3016ba566c3fdc21
presentKey <kim.hyeonjae4@gmail.com>
2023-01-30 02:16:22 +0900

    new: 제품 상세 페이지 구현

--- client/src/pages/ProductDetail.jsx
+++ client/src/pages/ProductDetail.jsx
@@ -1,5 +1,34 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import { useProductApi } from '../context/ProductApiContext';
+import ProductOrderForm from '../components/ProductOrderForm';
+import ProductDetailImage from '../components/ProductDetailImage';
+import ProductDetailInfo from '../components/ProductDetailInfo';
 
 export default function ProductDetail() {
-  return <div>ProductDetail</div>;
+  const { id } = useParams();
+  const { product } = useProductApi();
+  const {
+    isLoading,
+    error,
+    data: productDetail,
+  } = useQuery(['productDetail', id], () => product.getProduct(id), {
+    staleTime: 1000 * 60 * 5,
+  });
+  if (isLoading) return <p className="w-full">Loading...</p>;
+  if (error) return <p className="w-full">Something is wrong</p>;
+
+  const { imgFiles, detailInfo, name } = productDetail;
+
+  return (
+    <div className="w-full p-8">
+      <div className="flex mb-8 justify-between">
+        <ProductDetailImage imgFiles={imgFiles} name={name} />
+        <ProductOrderForm productDetail={productDetail} />
+      </div>
+      <div className="h-4 w-full bg-slate-300 my-8"></div>
+      <ProductDetailInfo detailInfo={detailInfo} name={name} />
+    </div>
+  );
 }
