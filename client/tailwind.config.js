04e3791b3d6dc374969f8b86af3ece0033c334ab
presentKey <kim.hyeonjae4@gmail.com>
2023-01-30 02:12:52 +0900

    style: 커스텀 width 설정

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        128: '32rem',
+      },
+    },
   },
   plugins: [],
 };
