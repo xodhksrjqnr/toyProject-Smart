c99ab5b349a46602a24fd312d4f2e699037740d4
presentKey <kim.hyeonjae4@gmail.com>
2023-02-01 20:10:32 +0900

    chore: StrictMode 적용

--- client/src/index.js
+++ client/src/index.js
@@ -31,7 +31,11 @@ const router = createBrowserRouter([
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<RouterProvider router={router} />);
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
