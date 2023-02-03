6295ca6e9a7157423dd1e8107cb1722055609666
xodhksrjqnr <xodhksrjqnr@naver.com>
2023-02-03 17:22:43 +0900

    디렉토리 이동

--- src/main/java/taewan/Smart/category/repository/CategoryItemRepository.java
+++
@@ -1,7 +0,0 @@
-package taewan.Smart.category.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import taewan.Smart.category.entity.CategoryItem;
-
-public interface CategoryItemRepository extends JpaRepository<CategoryItem, Long> {
-}
--- src/main/java/taewan/Smart/category/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package taewan.Smart.category.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import taewan.Smart.category.entity.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
