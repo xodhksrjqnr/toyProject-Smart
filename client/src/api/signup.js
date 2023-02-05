51dafb3c38f8a03594ab8fa3579409f2c7fe8faa
presentKey <kim.hyeonjae4@gmail.com>
2023-02-05 23:37:53 +0900

    fix: id의 name 변경

--- client/src/api/signup.js
+++ client/src/api/signup.js
@@ -4,14 +4,14 @@ export async function signup(info) {
   const { id, password, email, birthday, phoneNumber } = info;
   const data = new FormData();
 
-  data.append('id', id);
+  data.append('memberId', id);
   data.append('password', password);
   data.append('email', email);
   data.append('birthday', birthday);
   data.append('phoneNumber', phoneNumber);
 
   return axios
-    .post(process.env.REACT_APP_SIGNUP_URLL, data)
+    .post(process.env.REACT_APP_SIGNUP_URL, data)
     .then((data) => data)
     .catch((error) => error.response);
 }
