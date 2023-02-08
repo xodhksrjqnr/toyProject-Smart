7cc1642479335b847cbbdf5e18d46601e7917170
xodhksrjqnr <xodhksrjqnr@naver.com>
2023-02-08 18:53:45 +0900

    #2 MemberSaveDto valid 수정

--- backend/src/main/java/taewan/Smart/domain/member/dto/MemberSaveDto.java
+++ backend/src/main/java/taewan/Smart/domain/member/dto/MemberSaveDto.java
@@ -3,7 +3,6 @@ package taewan.Smart.domain.member.dto;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.lang.Nullable;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
@@ -16,14 +15,13 @@ public class MemberSaveDto {
 
     @NotEmpty(message = "아이디를 입력해야합니다.")
     private String memberId;
+    @NotEmpty(message = "이메일을 입력해야합니다.")
     @Email(message = "이메일 형식이 잘못되었습니다.")
     private String email;
     @NotEmpty(message = "패스워드를 입력해야합니다.")
     private String password;
-    @Nullable
-    @Pattern(regexp = "^010-[0-9]{4}-[0-9]{4}$", message = "핸드폰 번호 형식이 잘못되었습니다.")
+    @Pattern(regexp = "^(010-\\d{4}-\\d{4})$|^$", message = "핸드폰 번호 형식이 잘못되었습니다.")
     private String phoneNumber;
-    @Nullable
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthday;
 }
