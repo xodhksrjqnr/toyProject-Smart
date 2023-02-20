48a9d0f75e0ce3d21228bf83f456e2d38c97276c
xodhksrjqnr <xodhksrjqnr@naver.com>
2023-02-20 20:26:23 +0900

    #23 AuthFilter 구현 & WebConfig 필터 등록 및 addCorsMappings() 메서드 수정 & Interceptor 제거

--- backend/src/main/java/taewan/Smart/global/config/WebConfig.java
+++ backend/src/main/java/taewan/Smart/global/config/WebConfig.java
@@ -1,15 +1,29 @@
 package taewan.Smart.global.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import taewan.Smart.global.config.filter.AuthFilter;
+
+import javax.servlet.Filter;
 
 import static taewan.Smart.global.utils.PropertyUtil.*;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public FilterRegistrationBean<Filter> authFilter() {
+        FilterRegistrationBean<Filter> filter = new FilterRegistrationBean<>();
+
+        filter.setFilter(new AuthFilter());
+        filter.addUrlPatterns("/members/*", "/orders/*");
+        return filter;
+    }
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler(getAccessPath())
@@ -21,15 +35,6 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins(getClientAddress())
                 .allowedMethods("GET", "POST", "OPTIONS")
-                .allowCredentials(true).maxAge(5);
-    }
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new AuthInterceptor())
-                .addPathPatterns("/members/**", "/orders/**")
-                .excludePathPatterns(
-                        "/members/create", "/members/login", "/members/certificate/*"
-                );
+                .allowCredentials(true).maxAge(-1);
     }
 }
---
+++ backend/src/main/java/taewan/Smart/global/config/filter/AuthFilter.java
@@ -0,0 +1,50 @@
+package taewan.Smart.global.config.filter;
+
+import io.jsonwebtoken.JwtException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.Set;
+
+import static taewan.Smart.global.utils.JwtUtil.parseJwt;
+
+@Slf4j
+public class AuthFilter implements Filter {
+
+    private final Set<String> excludeUrl = Set.of(
+            "/members/create", "/members/login", "/members/certificate"
+    );
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+
+        try {
+            boolean flag = false;
+
+            for (String exclude : excludeUrl) {
+                flag = httpServletRequest.getRequestURI().contains(exclude) || flag;
+            }
+            if (!flag) {
+                parseJwt(httpServletRequest.getHeader(HttpHeaders.AUTHORIZATION));
+            }
+            chain.doFilter(request, response);
+        } catch (JwtException ex) {
+            log.error("JwtException : Request Method : {}, URL : {}", httpServletRequest.getMethod(), httpServletRequest.getRequestURI());
+            throw ex;
+        }
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
