4958b81671c845f4444e70ae057c522502346328
xodhksrjqnr <xodhksrjqnr@naver.com>
2023-03-28 14:02:40 +0900

    aop 폴더 제거

--- backend/src/main/java/taewan/Smart/aop/annotation/PathConverter.java
+++
@@ -1,9 +0,0 @@
-package taewan.Smart.aop.annotation;
-
-import java.lang.annotation.*;
-
-@Documented
-@Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.PARAMETER})
-public @interface PathConverter {
-}
